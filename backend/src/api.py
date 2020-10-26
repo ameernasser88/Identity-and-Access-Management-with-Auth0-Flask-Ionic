d22f7812fd75b67c4c308b48bd759f72fc83215b
ameernasser88 <34694069+ameernasser88@users.noreply.github.com>
2020-10-26 14:52:55 +0200

    removed instruction comments

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,14 +19,7 @@ CORS(app)
 # db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks', methods=['GET'])
 @cross_origin()
 def get_drinks():
@@ -44,14 +37,7 @@ def get_drinks():
         )
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+
 @app.route('/drinks-detail', methods=['GET'])
 @cross_origin()
 @requires_auth(permission='get:drinks-detail')
@@ -70,15 +56,6 @@ def get_drink_details(jwt):
             }
         )
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
 
 @app.route('/drinks', methods=['POST'])
 @cross_origin()
@@ -90,62 +67,42 @@ def create_drink(jwt):
     try:
         new_drink = Drink(title=new_title,recipe=str(new_recipe).replace("'", '"'))
         new_drink.insert()
-        drinks = Drink.query.order_by(Drink.id).all()
-        formatted_drinks = [drink.long() for drink in drinks]
+        formatted_drinks = []
+        formatted_drinks.append(new_drink.long())
         return jsonify({
             "success": True
             , "created": new_drink.id
             , "drinks": formatted_drinks
-            , "total_drinks": len(formatted_drinks)
         })
 
     except:
         abort(422)
 
-
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
-@app.route('/drinks/<drink_id>', methods=['PATCH'])
+@app.route('/drinks/<id>', methods=['PATCH'])
 @cross_origin()
 @requires_auth('patch:drinks')
-def update_drink(drink_id,jwt):
+def update_drink(id,jwt):
     try:
-        drink = Drink.query.get(drink_id)
+        drink = Drink.query.get(id)
+        if drink is None:
+            abort(404)
         body = request.get_json()
         new_title = body.get('title', drink.title)
         new_recipe = body.get('recipe', drink.recipe)
+        drink.title=new_title
+        drink.recipe=new_recipe
         drink.update()
-        drinks = Drink.query.order_by(Drink.id).all()
-        formatted_drinks = [drink.long() for drink in drinks]
+        formatted_drinks = []
+        formatted_drinks.append(drink.long())
         return jsonify({
             "success": True
-            , "created": drink.id
+            , "updated": drink.id
             , "drinks": formatted_drinks
-            , "total_drinks": len(formatted_drinks)
         })
     except:
         abort(422)
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks/<drink_id>', methods=['DELETE'])
 @cross_origin()
 @requires_auth('delete:drinks')
@@ -158,7 +115,7 @@ def delete_drink(drink_id,jwt):
             drink.delete()
             return jsonify({
                 "success": True
-                , "deleted": drink.id
+                , "delete": drink.id
             })
     except:
         abort(400)
